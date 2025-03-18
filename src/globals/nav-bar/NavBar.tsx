42137603caa127f63724e3be9e8b2b122bcb8060
Laure <laure@uxer.fr>
2025-03-18 16:35:24 +0100

    responsive navBar & menu burger

--- src/globals/nav-bar/NavBar.tsx
+++ src/globals/nav-bar/NavBar.tsx
@@ -2,9 +2,81 @@
 import Image from 'next/image';
 import styles from './NavBar.module.scss';
 import Link from 'next/link';
-import { Cat, HandHeart, House, User, Users } from '@phosphor-icons/react';
+import {
+   Cat,
+   HandHeart,
+   House,
+   List,
+   User,
+   Users,
+} from '@phosphor-icons/react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 const NavBar = () => {
+   const [isDesktop, setIsDesktop] = useState(false);
+   const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+   const handleOpenMenuBurger = useCallback(() => {
+      setMenuIsOpen(!menuIsOpen);
+   }, [menuIsOpen]);
+
+   useEffect(() => {
+      const handleResize = () => {
+         setIsDesktop(window.innerWidth >= 1024);
+      };
+      handleResize();
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+   }, []);
+
+   const renderNavItems = () => {
+      return (
+         <ul className={styles.nav}>
+            <li className={styles.item}>
+               <Link href="/home">
+                  <House weight="bold" />
+                  <span>Accueil</span>
+               </Link>
+            </li>
+            <li className={styles.item}>
+               <Link href="/pets">
+                  <Cat weight="bold" />
+                  <span>Nos animaux</span>
+               </Link>
+            </li>
+            <li className={styles.item}>
+               <Link href="/associations">
+                  <HandHeart weight="bold" />
+                  <span>Nos associations</span>
+               </Link>
+            </li>
+            <li className={styles.item}>
+               <Link href="/foster-families">
+                  <Users weight="bold" />
+                  <span>Nos familles d'accueil</span>
+               </Link>
+            </li>
+         </ul>
+      );
+   };
+
+   const renderNavBar = useMemo(() => {
+      if (isDesktop) return renderNavItems();
+
+      return (
+         <div className={styles.bugerMenu}>
+            <div className={styles.menuWrapper}>
+               <button type="button" onClick={handleOpenMenuBurger}>
+                  <List weight="bold" />
+               </button>
+               {menuIsOpen && (
+                  <div className={styles.menuOpen}>{renderNavItems()}</div>
+               )}
+            </div>
+         </div>
+      );
+   }, [isDesktop, handleOpenMenuBurger, menuIsOpen, renderNavItems]);
+
    return (
       <div className={styles.navbar}>
          <div className={styles.content}>
@@ -17,32 +89,7 @@ const NavBar = () => {
                />
             </Link>
             <div className={styles.container}>
-               <ul className={styles.nav}>
-                  <li className={styles.item}>
-                     <Link href={'/home'}>
-                        <House weight="bold" />
-                        <span>Accueil</span>
-                     </Link>
-                  </li>
-                  <li className={styles.item}>
-                     <Link href={'/pets'}>
-                        <Cat weight="bold" />
-                        <span>Nos animaux</span>
-                     </Link>
-                  </li>
-                  <li className={styles.item}>
-                     <Link href={'/associations'}>
-                        <HandHeart weight="bold" />
-                        <span>Nos associations</span>
-                     </Link>
-                  </li>
-                  <li className={styles.item}>
-                     <Link href={'/foster-families'}>
-                        <Users weight="bold" />
-                        <span> Nos familles d'accueil</span>
-                     </Link>
-                  </li>
-               </ul>
+               {renderNavBar}
                <button type="button" className={styles.user}>
                   <User weight="bold" />
                   <span>Mon espace</span>
