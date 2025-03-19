67e8e6bf3dae114793a93ccd320d3f4420d31635
Laure <laure@uxer.fr>
2025-03-19 18:21:29 +0100

    Reapply "create animals page & cards animal"
    This reverts commit 9fa3639cfd2f1276deb24f6e14dec50f801399a5.

---
+++ src/animals/card-animal/CardAnimal.tsx
@@ -0,0 +1,43 @@
+'use client';
+import Image from 'next/image';
+import styles from './CardAnimal.module.scss';
+import { useMemo } from 'react';
+import { GenderFemale, GenderMale } from '@phosphor-icons/react';
+
+type Props = {
+   pet: any;
+};
+
+const CardAnimal = ({ pet }: Props) => {
+   const renderGenger = useMemo(() => {
+      if (pet?.gender) {
+         return <GenderMale weight="bold" />;
+      }
+
+      if (!pet?.gender) {
+         return <GenderFemale weight="bold" />;
+      }
+   }, [pet]);
+
+   return (
+      <div className={styles.card}>
+         <div className={styles.top}>
+            <Image
+               src={'/images/kitten-3.jpg'}
+               alt="animal-picture"
+               width={335}
+               height={239}
+            />
+         </div>
+         <div className={styles.informations}>
+            <div className={styles.names}>
+               <p className={styles.animalName}>{pet.name}</p>
+               <p className={styles.assoName}>Nom de l'Association</p>
+            </div>
+            <span className={styles.gender}>{renderGenger}</span>
+         </div>
+      </div>
+   );
+};
+
+export default CardAnimal;
