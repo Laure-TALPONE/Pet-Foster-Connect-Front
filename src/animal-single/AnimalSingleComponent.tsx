b8ba0f829d161cc0dd9e50ddf502e7e61b5532d3
Laure <laure@uxer.fr>
2025-03-25 17:11:41 +0100

    feat: correction settings slider

--- src/animal-single/AnimalSingleComponent.tsx
+++ src/animal-single/AnimalSingleComponent.tsx
@@ -9,7 +9,7 @@ const AnimalSingleComponent = () => {
       responsive: [
          {
             breakpoint: 9999,
-            settings: 'unslick',
+            settings: { unslick: true },
          },
          {
             breakpoint: 1024,
