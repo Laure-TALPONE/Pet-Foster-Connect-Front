8e2fc8ac0f7e0a268cfa44f551062a9ff5558dd2
Justine Meunier <justine.meunier.design@gmail.com>
2025-04-07 15:03:30 +0200

    feat: integration dynamic with fake data for association card

--- src/associations/card-association/CardAssociation.tsx
+++ src/associations/card-association/CardAssociation.tsx
@@ -13,15 +13,15 @@ const CardAssociation = ({ asso }: Props) => {
          <div className={styles.top}>
             <Image
                src={'/images/logo-association.webp'}
-               alt="animal-picture"
+               alt="association-picture"
                width={335}
                height={239}
             />
          </div>
          <div className={styles.informations}>
             <div className={styles.names}>
-               <p className={styles.assoName}>Nom de l'Association</p>
-               <p className={styles.city}>Ville (33)</p>
+               <p className={styles.assoName}>{asso.name}</p>
+               <p className={styles.city}>{asso.location} ({asso.postalcode.slice(0, 2)})</p>
             </div>
          </div>
       </div>
