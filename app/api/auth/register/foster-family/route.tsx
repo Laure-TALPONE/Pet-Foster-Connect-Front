52e49a0992edb071c432065b062608b5ae067ce0
Laure <laure@uxer.fr>
2025-04-14 16:06:25 +0200

    feat: add useform

---
+++ app/api/auth/register/foster-family/route.tsx
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+   try {
+      const data = await request.json();
+
+      const response = await fetch(
+         // 'http://jeremyjacquette-server.eddi.cloud/api/organization/register',
+         'http://localhost/api/foster-family/register',
+         {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+         }
+      );
+
+      const result = await response.json();
+
+      if (!response.ok) {
+         return NextResponse.json(
+            { message: result.message || 'Erreur côté serveur NestJS' },
+            { status: response.status }
+         );
+      }
+
+      return NextResponse.json(result, { status: 200 });
+   } catch (error: any) {
+      console.error('Erreur lors du POST vers NestJS :', error);
+      return NextResponse.json(
+         { message: 'Erreur lors de l’inscription', error: error.message },
+         { status: 500 }
+      );
+   }
+}
--- src/subcribe/foster-family/SubscriptionFamily.tsx
+++ src/subcribe/foster-family/SubscriptionFamily.tsx
@@ -2,11 +2,129 @@
 
 import Image from 'next/image';
 import styles from './SubscriptionFamily.module.scss';
+import { useForm } from 'react-hook-form';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { Eye, EyeClosed, FileArrowDown } from '@phosphor-icons/react';
+import Link from 'next/link';
+import ModalComponent from '@/globals/components/modal/ModalComponent';
+import ModalLoginComponent from '@/login/ModalLoginComponent';
 
 const SubcriptionFamily = () => {
+   const {
+      register,
+      setValue,
+      handleSubmit,
+      watch,
+      formState: { errors },
+   } = useForm();
+   const [errorMessage, setErrorMessage] = useState('');
+   const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+   const [confirmVisible, setConfirmVisible] = useState<boolean>(false);
+   const [openModalLogin, setOpenModalLogin] = useState<boolean>(false);
+   const watchPassword = watch('password');
+   const watchConfirm = watch('confirm');
+
+   const handleDisplayPassword = useCallback(
+      (item: string) => {
+         if (item === 'password') {
+            setPasswordVisible(!passwordVisible);
+         }
+
+         if (item === 'confirm') {
+            setConfirmVisible(!confirmVisible);
+         }
+      },
+      [passwordVisible, confirmVisible]
+   );
+
+   useEffect(() => {
+      if (watchPassword && watchPassword.length >= 2) {
+         const lowerCaseRegex = /[a-z]/;
+         const uppercaseRegex = /[A-Z]/;
+         const digitRegex = /[0-9]/;
+         const specialCharRegex = /[^A-Za-z0-9.,]/;
+
+         if (watchPassword.length < 7) {
+            setErrorMessage('Votre mot de passe est trop court !');
+         } else if (!watchPassword.match(lowerCaseRegex)) {
+            setErrorMessage('Votre mot de passe doit contenir une minuscule !');
+         } else if (!watchPassword.match(uppercaseRegex)) {
+            setErrorMessage('Votre mot de passe doit contenir une majuscule !');
+         } else if (!watchPassword.match(digitRegex)) {
+            setErrorMessage('Votre mot de passe doit contenir un chiffre !');
+         } else if (!watchPassword.match(specialCharRegex)) {
+            setErrorMessage(
+               'Votre mot de passe doit contenir un caratère spécial : @#! !'
+            );
+         } else {
+            setErrorMessage('');
+         }
+      }
+   }, [watchPassword]);
+
+   const onSubmit = async (data: any) => {
+      const newData = {
+         user: {
+            email: data.email,
+            password: watchPassword === watchConfirm && data.password,
+         },
+         foster: {
+            lastname: data.lastname,
+            firstname: data.firstname,
+            address: data.address,
+            city: data.city,
+            postcode: data.postcode,
+            phone: data.phone,
+            description: data.description,
+            image: 'https://www.solidarite-peuple-animal.com/data/document/1/22.800.jpg?1744636663',
+         },
+      };
+
+      console.log(newData, 'ici les datas');
+
+      // try {
+      //    const response = await fetch('/api/auth/register/foster-family', {
+      //       method: 'POST',
+      //       headers: {
+      //          'Content-Type': 'application/json',
+      //       },
+      //       body: JSON.stringify(newData),
+      //    });
+
+      //    const result = await response.json();
+
+      //    if (!response.ok) {
+      //       // setErrorEmailMessage(result.message);
+      //       throw new Error(result.message || 'Une erreur est survenue.');
+      //    }
+
+      //    if (response.ok) {
+      //       console.log('Inscription réussie :', result);
+      //       setOpenModalLogin(true);
+
+      //    }
+      // } catch (error) {
+      //    console.error('Erreur API :', error);
+      // }
+   };
+
+   const renderModalLogin = useMemo(() => {
+      if (openModalLogin) {
+         return (
+            <ModalComponent
+               onClose={setOpenModalLogin(false)}
+               children={
+                  <ModalLoginComponent onClose={setOpenModalLogin(false)} />
+               }
+            />
+         );
+      }
+   }, [openModalLogin]);
+
    return (
       <section className="container">
          <div className={styles.content}>
+            {renderModalLogin}
             <section className={styles.informations}>
                <div className={styles.infos}>
                   <h2 className={styles.title}>
@@ -36,38 +154,185 @@ const SubcriptionFamily = () => {
                </div>
             </section>
 
-            <section className={styles.form}>
+            <form className={styles.form}>
                <h2>Devenez famille d’accueil et offrez un refuge temporaire</h2>
                <div className={styles.inputs}>
-                  <div className="m-input m-input__background">
-                     <input type="text" placeholder="Nom" />
-                  </div>
-                  <div className="m-input m-input__background">
-                     <input type="text" placeholder="Prénom" />
-                  </div>
-                  <div className="m-input m-input__background">
-                     <input type="text" placeholder="Adresse" />
-                  </div>
-                  <div className="m-input m-input__background">
-                     <input type="text" placeholder="Code postal" />
-                  </div>
-                  <div className="m-input m-input__background">
-                     <input type="text" placeholder="Ville" />
-                  </div>
-                  <div className="m-input m-input__background">
-                     <input type="text" placeholder="E-mail" />
-                  </div>
-                  <div className="m-input m-input__background">
-                     <input type="text" placeholder="Téléphone" />
-                  </div>
-                  <div className="m-input m-input__background">
-                     <textarea placeholder="Description" />
-                  </div>
+                  <section className={styles.contacts}>
+                     <section className={styles.email}>
+                        <div
+                           className={
+                              errors.email
+                                 ? 'm-input m-input__background m-input__error'
+                                 : 'm-input m-input__background'
+                           }
+                        >
+                           <input
+                              type="email"
+                              placeholder="E-mail de connexion"
+                              {...register('email', { required: true })}
+                           />
+                        </div>
+                        {/* {errorEmailMessage && (
+                        <p className={styles.emailError}>{errorEmailMessage}</p>
+                     )} */}
+                     </section>
+                     <section className={errorMessage ? styles.password : ''}>
+                        <div
+                           className={
+                              errors.password
+                                 ? 'm-input m-input__background m-input__error'
+                                 : 'm-input m-input__background'
+                           }
+                        >
+                           <input
+                              type={passwordVisible ? 'text' : 'password'}
+                              placeholder="Mot de passe"
+                              {...register('password', { required: true })}
+                           />
+                           <button
+                              type="button"
+                              className="m-input__suffix"
+                              onClick={() => handleDisplayPassword('password')}
+                           >
+                              {passwordVisible ? (
+                                 <EyeClosed weight="bold" />
+                              ) : (
+                                 <Eye weight="bold" />
+                              )}
+                           </button>
+                        </div>
+                        {errorMessage && (
+                           <p className={styles.passwordError}>
+                              {errorMessage}
+                           </p>
+                        )}
+                     </section>
+                     <div
+                        className={
+                           watchPassword !== watchConfirm
+                              ? 'm-input m-input__background m-input__error'
+                              : 'm-input m-input__background'
+                        }
+                     >
+                        <input
+                           type={confirmVisible ? 'text' : 'password'}
+                           placeholder="Confirmer le mot de passe"
+                           {...register('confirm')}
+                        />
+                        <button
+                           type="button"
+                           className="m-input__suffix"
+                           onClick={() => handleDisplayPassword('confirm')}
+                        >
+                           {confirmVisible ? (
+                              <EyeClosed weight="bold" />
+                           ) : (
+                              <Eye weight="bold" />
+                           )}
+                        </button>
+                     </div>
+                  </section>
+                  <section className={styles.family}>
+                     <div
+                        className={
+                           errors.lastname
+                              ? 'm-input m-input__background m-input__error'
+                              : 'm-input m-input__background'
+                        }
+                     >
+                        <input
+                           type="text"
+                           placeholder="Nom"
+                           {...register('lastname', { required: true })}
+                        />
+                     </div>
+                     <div
+                        className={
+                           errors.firstname
+                              ? 'm-input m-input__background m-input__error'
+                              : 'm-input m-input__background'
+                        }
+                     >
+                        <input
+                           type="text"
+                           placeholder="Prénom"
+                           {...register('firstname', { required: true })}
+                        />
+                     </div>
+                     <div
+                        className={
+                           errors.address
+                              ? 'm-input m-input__background m-input__error'
+                              : 'm-input m-input__background'
+                        }
+                     >
+                        <input
+                           type="text"
+                           placeholder="Adresse"
+                           {...register('address', { required: true })}
+                        />
+                     </div>
+                     <div
+                        className={
+                           errors.postcode
+                              ? 'm-input m-input__background m-input__error'
+                              : 'm-input m-input__background'
+                        }
+                     >
+                        <input
+                           type="text"
+                           placeholder="Code postal"
+                           {...register('postcode', { required: true })}
+                        />
+                     </div>
+                     <div
+                        className={
+                           errors.city
+                              ? 'm-input m-input__background m-input__error'
+                              : 'm-input m-input__background'
+                        }
+                     >
+                        <input
+                           type="text"
+                           placeholder="Ville"
+                           {...register('city', { required: true })}
+                        />
+                     </div>
+                     <div
+                        className={
+                           errors.phone
+                              ? 'm-input m-input__background m-input__error'
+                              : 'm-input m-input__background'
+                        }
+                     >
+                        <input
+                           type="text"
+                           placeholder="Téléphone"
+                           {...register('phone', { required: true })}
+                        />
+                     </div>
+                     <div
+                        className={
+                           errors.description
+                              ? 'm-input m-input__background m-input__error'
+                              : 'm-input m-input__background'
+                        }
+                     >
+                        <textarea
+                           placeholder="Description"
+                           {...register('description', { required: true })}
+                        />
+                     </div>
+                  </section>
                </div>
-               <button type="button" className="m-button">
+               <button
+                  type="submit"
+                  className="m-button"
+                  onClick={handleSubmit(onSubmit)}
+               >
                   Valider l’inscription
                </button>
-            </section>
+            </form>
 
             <section className={styles.register}>
                <div className={styles.left}>
@@ -78,9 +343,9 @@ const SubcriptionFamily = () => {
                      animaux dans le besoin !
                   </p>
                </div>
-               <button type="button" className="m-button">
+               <Link href={'/inscription/association'} className="m-button">
                   M’inscrire ici
-               </button>
+               </Link>
             </section>
          </div>
       </section>
