d9c2778b42dfffffae6c80fe8fe7bb561bca663d
Laure <laure@uxer.fr>
2025-04-16 15:09:50 +0200

    feat: update footer & css

--- src/globals/components/footer/FooterComponent.tsx
+++ src/globals/components/footer/FooterComponent.tsx
@@ -4,12 +4,17 @@ import styles from './FooterComponent.module.scss';
 
 const FooterComponent = () => {
    return (
-      <div className={styles.footer}>
-         <p>
-            <Link href={'/mentions-legales'}>Mentions légales</Link> | Pet
-            Foster Connect  © 2025
-         </p>
-      </div>
+      <section className={styles.footer}>
+         <div className={styles.links}>
+            <Link href={'/mentions-legales'}>Mentions légales</Link>
+            <Link href={'/cookies'}>Cookies</Link>
+            <Link href={'/politique-confidentialite'}>
+               Politique de confidentialité
+            </Link>
+         </div>
+         <span>|</span>
+         <p className={styles.copyright}>Pet Foster Connect © 2025</p>
+      </section>
    );
 };
 
