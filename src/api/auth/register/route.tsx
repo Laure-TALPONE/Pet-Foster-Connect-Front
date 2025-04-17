68eafd4281f76a911f4a6eff11086977873534f5
Laure <laure@uxer.fr>
2025-04-17 15:22:37 +0200

    feat: add controller register

---
+++ src/api/auth/register/route.tsx
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const fetchPostRegister = async (request: NextRequest) => {
+   try {
+      const data = await request.json();
+
+      const response = await fetch(
+         // 'http://jeremyjacquette-server.eddi.cloud/api/organization/register',
+         'http://localhost/api/organization/register',
+         {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+         }
+      );
+
+      const result = await response.json();
+
+      if (!response.ok) {
+         return NextResponse.json(
+            { message: result.message || 'Erreur côté serveur NestJS' },
+            { status: response.status }
+         );
+      }
+
+      return NextResponse.json(result, { status: 200 });
+   } catch (error: any) {
+      console.error('Erreur lors du POST vers NestJS :', error);
+      return NextResponse.json(
+         { message: 'Erreur lors de l’inscription', error: error.message },
+         { status: 500 }
+      );
+   }
+};
+
+export default fetchPostRegister;
